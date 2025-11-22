2d0c50620b9716866db65d59e8b46656c797b561
SSR-001 <ssrichardson1010@gmail.com>
2025-11-22 12:14:30 +0000

    add cloudinary

--- my_project/settings.py
+++ my_project/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_1'
+    'cloudinary_storage',
+    'app_1',
 ]
 
 MIDDLEWARE = [
